91176b3969118f31007d60a9f8d096806e6f4940
SirJUST <theoperaking@yahoo.com>
2019-02-21 14:33:32 -0500

    successfully created 3 lists with data from spreadsheet, ready to import into write function

--- TruncateSpreadsheet/Program.cs
+++ TruncateSpreadsheet/Program.cs
@@ -11,7 +11,7 @@ namespace TruncateSpreadsheet
         static void Main(string[] args)
         {
             readFile read = new readFile();
-            List<Tradesman> cutList = read.readSpreadSheet(FilePaths.readPath);
+            List<List<Tradesman>> cutList = read.readSpreadSheet(FilePaths.readPath);
             writeFile write = new writeFile();
             write.WriteSpreadSheet(cutList, FilePaths.writePath);
             Console.WriteLine("Truncation complete.");
--- TruncateSpreadsheet/writeFile.cs
+++ TruncateSpreadsheet/writeFile.cs
@@ -10,7 +10,7 @@ namespace TruncateSpreadsheet
 {
     class writeFile
     {
-        public void WriteSpreadSheet(List<Tradesman> licenses, string path)
+        public void WriteSpreadSheet(List<List<Tradesman>> licenses, string path)
         {
             var myFileInfo = new FileInfo(path);
             using (ExcelPackage package = new ExcelPackage())
@@ -27,19 +27,19 @@ namespace TruncateSpreadsheet
                 worksheet.Cells["H" + cellCounter].Value = "Zip";
                 worksheet.Cells["I" + cellCounter].Value = "ExpirationDate";
                 cellCounter++;
-                foreach (Tradesman licenseHolder in licenses)
-                {
-                    worksheet.Cells["A" + cellCounter].Value = licenseHolder.LicenseType;
-                    worksheet.Cells["B" + cellCounter].Value = licenseHolder.LicenseNumber;
-                    worksheet.Cells["C" + cellCounter].Value = licenseHolder.Name;
-                    worksheet.Cells["D" + cellCounter].Value = licenseHolder.Address1;
-                    worksheet.Cells["E" + cellCounter].Value = licenseHolder.Address2;
-                    worksheet.Cells["F" + cellCounter].Value = licenseHolder.City;
-                    worksheet.Cells["G" + cellCounter].Value = licenseHolder.State;
-                    worksheet.Cells["H" + cellCounter].Value = licenseHolder.Zip;
-                    worksheet.Cells["I" + cellCounter].Value = licenseHolder.ExpirationDate;
-                    cellCounter++;
-                }
+                //foreach (Tradesman licenseHolder in licenses)
+                //{
+                //    worksheet.Cells["A" + cellCounter].Value = licenseHolder.LicenseType;
+                //    worksheet.Cells["B" + cellCounter].Value = licenseHolder.LicenseNumber;
+                //    worksheet.Cells["C" + cellCounter].Value = licenseHolder.Name;
+                //    worksheet.Cells["D" + cellCounter].Value = licenseHolder.Address1;
+                //    worksheet.Cells["E" + cellCounter].Value = licenseHolder.Address2;
+                //    worksheet.Cells["F" + cellCounter].Value = licenseHolder.City;
+                //    worksheet.Cells["G" + cellCounter].Value = licenseHolder.State;
+                //    worksheet.Cells["H" + cellCounter].Value = licenseHolder.Zip;
+                //    worksheet.Cells["I" + cellCounter].Value = licenseHolder.ExpirationDate;
+                //    cellCounter++;
+                //}
                 try
                 {
                     package.SaveAs(myFileInfo);
