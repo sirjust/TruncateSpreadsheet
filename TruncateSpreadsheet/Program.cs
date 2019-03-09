a58764b3d4d8b55ea53a8ce70282d1441775212d
SirJUST <theoperaking@yahoo.com>
2019-03-08 22:18:50 -0500

    add choice for two types of truncation

--- TruncateSpreadsheet/Program.cs
+++ TruncateSpreadsheet/Program.cs
@@ -10,11 +10,39 @@ namespace TruncateSpreadsheet
     {
         static void Main(string[] args)
         {
-            readFile_8090 read = new readFile_8090();
-            List<List<Tradesman>> cutList = read.readSpreadSheet(FilePaths.readPath);
-            List<List<Tradesman>> tradesmanList_8090 = read.produce8090List(cutList);
-            writeFile write = new writeFile();
-            write.WriteSpreadSheet(tradesmanList_8090, FilePaths.writePath);
+            Console.WriteLine("This program can truncate the master spreadsheet in one of two ways.");
+            bool correctSelection = false;
+            while (!correctSelection)
+            {
+                Console.WriteLine("To produce a spreadsheet from 0-90 days, press 1.");
+                Console.WriteLine("To produce a sheet for 80-90 days, press 2.");
+                string answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        {
+                            readFile read = new readFile();
+                            List<List<Tradesman>> cutList = read.readSpreadSheet(FilePaths.readPath);
+                            writeFile write = new writeFile();
+                            write.WriteSpreadSheet(cutList, FilePaths.writePath);
+                            correctSelection = true;
+                            break;
+                        }
+                    case "2":
+                        {
+                            readFile_8090 read = new readFile_8090();
+                            List<List<Tradesman>> cutList = read.readSpreadSheet(FilePaths.readPath);
+                            List<List<Tradesman>> tradesmanList_8090 = read.produce8090List(cutList);
+                            writeFile write = new writeFile();
+                            write.WriteSpreadSheet(tradesmanList_8090, FilePaths.writePath);
+                            correctSelection = true;
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("Please enter 1 or 2.");
+                        break;
+                }
+            }
             Console.WriteLine("Truncation complete.");
             Console.Read();
         }
